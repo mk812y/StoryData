274af9f39d30939910c2b010f501badcf22bdd28
mk812y <mk812y@gmail.com>
2022-08-25 18:09:04 +0300

    Initial Commit

---
+++ StoryData/StoryDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoryDataApp.swift
+//  StoryData
+//
+//  Created by Михаил Куприянов on 25.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct StoryDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
